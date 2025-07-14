94219b5c3dae947139aaf5a4701c298beb383ba9
SoberingRealityCheck <buzzs@Nightblood.localdomain>
2025-07-14 00:33:40 -0500

    fixed formatting error

--- hexapod-app/src/config/network.ts
+++ hexapod-app/src/config/network.ts
@@ -1,3 +1,5 @@
+import { env } from "process";
+
 export const networkConfig = {
   // Camera Stream Configuration
   camera: {
@@ -17,7 +19,7 @@ export const networkConfig = {
   api: {
     baseUrl: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api',
     robotStateUrl: 'https://arachne-1.claysawesomewebsite.uk',
-    apiKey: process.env.NEXT_PUBLIC_API_KEY || '',
+    apiKey: process.env.NEXT_PUBLIC_API_KEY || env.NEXT_PUBLIC_API_KEY,
     timeout: 30000, // 30 seconds
   },
 
